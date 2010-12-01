f671828a29270de4041c63f56787d78bcb6d52d2
cerbero <cerbero@0c3ba4e0-eb2a-11df-87e5-a1f122fe758f>
2010-12-01 11:00:59 +0000

    aggiunto controllo validità url e controllo captcha
    git-svn-id: http://forge.evonove.it/svn/shorty/trunk@29 0c3ba4e0-eb2a-11df-87e5-a1f122fe758f

--- src/main.py
+++ src/main.py
@@ -6,14 +6,23 @@ from api.handlers import ShortHandler, ExpandHandler
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
-
+from google.appengine.api import urlfetch
+from os import environ
+import captcha
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         greetings="parametro"
+        
+        chtml = captcha.displayhtml(
+                                    public_key = "6LfAO78SAAAAAHsj8mGWXKvrG8QWxMBWznhLZxTe",
+                                    use_ssl = False,
+                                    error = None)
+        
 
         template_values = {
             'greetings': greetings,
+             'captchahtml': chtml,
            }
 
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
@@ -24,18 +33,39 @@ class Result(webapp.RequestHandler):
         methods = self.request.get('core')
         url = self.request.get('url')
         pers = self.request.get('custom_hash')
-      
         
-        if methods=="classic":
-            res = formats.DecodeJson(core.core_classic(url))
-            list_url = res.getMessage()
-        elif methods=="custom":
-            res = formats.DecodeJson(core.core_custum(url,pers))
-            list_url = res.getMessage()
-        else :
-            res = "non trovato"
-            list_url = res
-        
+        challenge = self.request.get('recaptcha_challenge_field')
+        response  = self.request.get('recaptcha_response_field')
+        remoteip  = environ['REMOTE_ADDR']
+         
+        cResponse = captcha.submit(challenge,response,"6LfAO78SAAAAANHiQxEObRo36llHrkQa3kauMaD3",remoteip)
+
+        list_url="Errore"
+        if cResponse.is_valid:
+                try :
+                    if url[0:7]!="http://":
+                        url ="http://"+url
+                    result = urlfetch.fetch(url=url,method=urlfetch.GET)
+                    if result.status_code == 200:
+                        if methods=="classic":
+                            res = formats.DecodeJson(core.core_classic(url))
+                            list_url = res.getMessage()
+                        elif methods=="custom":
+                            res = formats.DecodeJson(core.core_custum(url,pers))
+                            list_url = res.getMessage()
+                        else :
+                            res = "non trovato"
+                            list_url = res
+                    elif result.status_code==300 or result.status_code==301 or result.status_code==302 or result.status_code==303 or result.status_code==307:
+                        res = "redirect"
+                        list_url = res
+                except :
+                    res = "Indirizzo non valido"
+                    list_url = res
+        else:
+            list_url = cResponse.error_code
+
+                
         template_values = {
             'urls': list_url,
            }
