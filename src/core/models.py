4c9d355c85cb8a23b821579ac5276d7308823483
masci <masci@0c3ba4e0-eb2a-11df-87e5-a1f122fe758f>
2010-12-08 00:01:59 +0000

    fixed model datatypes
    git-svn-id: http://forge.evonove.it/svn/shorty/trunk@33 0c3ba4e0-eb2a-11df-87e5-a1f122fe758f

--- src/core/models.py
+++ src/core/models.py
@@ -1,11 +1,4 @@
-import sys 
-import cgi 
-
-from google.appengine.api import users 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
-
 
 #cambiare filosofia quando inserisco un associazione 
 #tengo l'ultimo assegnamento come stringa  e il dominio
@@ -15,34 +8,36 @@ from google.appengine.ext import db
 class DomainTable (db.Model):
     domain = db.StringProperty(multiline=False)
     last_assignament = db.StringProperty(multiline=False)
-    
-# il flag active dice che l'associazione e' attiva . Quando faccio un
-#nuovo bindings per quel dominio vado a vedere se c'e' n'e' uno disattivo 
-#e gli asssegno quello 
+
 
 class UrlBox(db.Model):
+    """il flag active dice che l'associazione e' attiva . Quando faccio un
+    nuovo bindings per quel dominio vado a vedere se c'e' n'e' uno disattivo 
+    e gli asssegno quello
+    
+    Fields
+        TODO: docstrings 
+    
+    """
     domain = db.StringProperty(multiline=False)
-    url = db.StringProperty(multiline=False)
-    shorted_url = db.StringProperty(multiline=False)
-    date = db.FloatProperty()
-    active = db.BooleanProperty()
-    last_click = db.FloatProperty()
+    url = db.LinkProperty()
+    shorted_url = db.LinkProperty()
+    date = db.DateTimeProperty(auto_now=True)
+    active = db.BooleanProperty(default=True)
+    last_click = db.DateTimeProperty()
+
 
 class WhiteList(db.Model):
     domain = db.StringProperty(multiline=False)
     note = db.StringProperty(multiline=False)
 #nuovi classi per gestire gli altri servizi di shortener  
+
  
 class BlackListShorty(db.Model):
     domain = db.StringProperty(multiline=False)
 
+
 class Admin(db.Model):
     user = db.StringProperty(multiline=False)
     password = db.StringProperty(multiline=False)
-    
-     
 
-
-    
-    
-    
